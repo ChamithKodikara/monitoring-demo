3ee01a99bd3d71cc5edb3e0e0b6ae119c80ae998
Chamith Kodikara <chamith@ceylonit.com>
2019-04-01 16:43:29 +0530

    -api documentation samples

--- src/main/java/com/helixz/monitoring/demo/controller/CustomerController.java
+++ src/main/java/com/helixz/monitoring/demo/controller/CustomerController.java
@@ -5,6 +5,8 @@ import com.helixz.monitoring.demo.dto.response.CustomerCreateResponse;
 import com.helixz.monitoring.demo.dto.response.CustomerViewResponse;
 import com.helixz.monitoring.demo.entity.Customer;
 import com.helixz.monitoring.demo.service.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,7 @@ import java.util.Optional;
 /**
  * @author Chamith
  */
+@Api("Sample Customer Rest API Documentation")
 @RestController
 @RequestMapping("customer")
 public class CustomerController {
@@ -27,6 +30,7 @@ public class CustomerController {
     @Autowired
     private ModelMapper modelMapper;
 
+    @ApiOperation("Customer create API Documentation is here")
     @PostMapping
     public ResponseEntity<CustomerCreateResponse> create(@Validated @RequestBody CustomerCreateRequest request) {
         Customer customer = modelMapper.map(request, Customer.class);
@@ -35,6 +39,8 @@ public class CustomerController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+
+    @ApiOperation("Customer search API Documentation is here")
     @GetMapping(value = "/{id}")
     public ResponseEntity<CustomerViewResponse> create(@PathVariable Long id) {
         Optional<Customer> customer = customerService.retrieveCustomerById(id);
