ac28398a8cfdbbd1b6a33808e6b7ce8db73b452d
Chamith Kodikara <chamith@ceylonit.com>
2019-04-10 12:23:33 +0530

    -swagger config code cleanup

--- src/main/java/com/helixz/monitoring/demo/DemoApplication.java
+++ src/main/java/com/helixz/monitoring/demo/DemoApplication.java
@@ -3,8 +3,14 @@ package com.helixz.monitoring.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author Chamith
+ */
 
+@EnableSwagger2
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/helixz/monitoring/demo/config/AppConfig.java
+++ src/main/java/com/helixz/monitoring/demo/config/AppConfig.java
@@ -3,18 +3,14 @@ package com.helixz.monitoring.demo.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Chamith
  */
-@EnableJpaAuditing
-@EnableSwagger2
 @Configuration
 public class AppConfig {
 
